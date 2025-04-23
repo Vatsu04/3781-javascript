4447e03279091e4fd620a9955c81507029fdc194
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-04-23 19:54:37 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -10,6 +10,17 @@ const api = {
     }
   },
 
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/%{id}`)
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
   async salvarPensamento(pensamento) {
     try {
       const response = await fetch('http://localhost:3000/pensamentos', {
@@ -22,7 +33,24 @@ const api = {
       return await response.json()
     }
     catch {
-      alert('Erro ao buscar pensamentos')
+      alert('Erro ao salvar pensamentos')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao editar pensamentos')
       throw error
     }
   }
